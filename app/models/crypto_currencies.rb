a60614b87e3ff0dd7f5d57a540258938ee510a34
clemYvars <clem.yvars@gmail.com>
2022-06-30 21:55:10 +0200

    cryptov2 modif

--- app/models/crypto_currencies.rb
+++ app/models/crypto_currencies.rb
@@ -27,7 +27,7 @@ end
     # puts(test.main())
     puts('Le cours actuel du '"#{test.getPrice('eth')['data']['base']} est de #{test.getPrice('eth')['data']['amount']}$")
 
-$achatcrypto = .....#je sais pas par quoi remplir  mais le prix au moment ou il a achete la crypto
+$achatcrypto = #je sais pas par quoi remplir  mais le prix au moment ou il a achete la crypto
 $pourcentage = 10   # exemple si on veut un calcul avec 10% de perte ou augmentation
 
 if $achactcrypto <= $achatcrypto * (1 - $pourcentage/100)
